f16af97ffa0982f3a7f5616a8ca61bcb1fb8adc7
mishina naoya <msnyxx811@gmail.com>
2023-07-16 23:37:08 +0900

    sec_6fin

--- section_6/my-app/src/App.js
+++ section_6/my-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from './components/TestComponent';
+import GetItemComponent from './components/GetItemComponent';
+import EmotionComponent from './components/EmotionComponent';
 
 function App() {
   return (
     <div className="App">
       <TestComponent/>
+      <GetItemComponent/>
+      <EmotionComponent/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
