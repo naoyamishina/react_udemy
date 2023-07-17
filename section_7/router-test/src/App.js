201e4934ae97ee6b95d38b93346d5a8c2a1ba7cc
mishina naoya <msnyxx811@gmail.com>
2023-07-17 11:12:08 +0900

    sec7fin

--- section_7/router-test/src/App.js
+++ section_7/router-test/src/App.js
@@ -5,6 +5,9 @@ import Contact from './pages/Contact';
 import Information from './pages/Information';
 import NotFound from './pages/NotFound';
 import Layout from './Layout';
+import BookList from "./pages/BookList";
+import BookIndex from "./pages/BookIndex"; 
+import BookDetail from "./pages/BookDetail";
 import { Routes, Route, NavLink, Navigate} from 'react-router-dom';
 
 function App() {
@@ -17,6 +20,10 @@ function App() {
         <Route path="/information" element={<Information />} /> 
         <Route path="/contact" element={<Contact />} />
         <Route path="/auth" element={ isAuthenticated ? <Navigate to="/contact" /> : <Home /> } />
+        <Route path="/book-list" element={<BookList />} > // url用 
+          <Route index element={<BookIndex />} /> // 一覧表示 
+          <Route path=":id" element={<BookDetail />}/> // 詳細表示
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
